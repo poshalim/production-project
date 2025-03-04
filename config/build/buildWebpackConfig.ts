3d154395963454036c875274b476ea6b495c4087
is.kazantcev <is.kazantcev@severstal.com>
2025-03-04 11:53:48 +0300

    add eslint, fix all files

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,12 +1,12 @@
-import { BuildOptions } from './types/config'
-import webpack from 'webpack'
-import { buildPlugins } from './buildPlugins'
-import { buildLoaders } from './buildLoaders'
-import { buildResolvers } from './buildResolvers'
-import { buildDevServer } from './buildDevServer'
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-  const { paths, mode, isDev } = options
+  const { paths, mode, isDev } = options;
 
   return {
     mode,
@@ -20,8 +20,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
-  }
+  };
 }
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+
+const AppRouter = () => (
+  <Suspense fallback={<div>Loading</div>}>
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={(<div className="page-wrapper">{element}</div>)}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
+
+export default AppRouter;
