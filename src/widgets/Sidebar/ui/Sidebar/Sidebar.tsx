811cb94461afff885a9e3d4cb71aa19dc7d15728
is.kazantcev <is.kazantcev@severstal.com>
2025-03-05 07:53:13 +0300

    Add jest, add first test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -17,7 +17,15 @@ export const Sidebar = ({ className }: SidebarProps) => {
   };
 
   return (
-    <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+    <div
+      className={
+        classNames(
+          cls.Sidebar,
+          { [cls.collapsed]: collapsed },
+          [className],
+        )
+      }
+    >
       <Button onClick={onToggle}>toggle</Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
