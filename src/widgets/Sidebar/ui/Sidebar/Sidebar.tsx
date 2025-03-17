7d0981e29cba7dd7f7e59036d54c92b9e5afe0ba
is.kazantcev <is.kazantcev@severstal.com>
2025-03-17 08:14:02 +0300

    add react testing library, add tests for btn, sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -18,6 +18,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
   return (
     <div
+      data-testid="sidebar"
       className={
         classNames(
           cls.Sidebar,
@@ -26,7 +27,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
         )
       }
     >
-      <Button onClick={onToggle}>toggle</Button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>toggle</Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
