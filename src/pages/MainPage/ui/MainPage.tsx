a21940c77e7241e3ff5d90b9ca96142d28108f85
is.kazantcev <is.kazantcev@severstal.com>
2025-03-20 09:03:55 +0300

    update

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,4 +1,4 @@
-import { BugButton } from 'app/providers/ErrorBoundary'
+import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
