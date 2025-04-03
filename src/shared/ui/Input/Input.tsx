0d8c64d794e9cf029457ebe3cef768c2dc643271
is.kazantcev <is.kazantcev@severstal.com>
2025-04-03 15:55:55 +0300

    add new input, add login, form ui

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,81 @@
+import {
+  ChangeEvent, InputHTMLAttributes, memo, useEffect, useRef, useState,
+} from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Input.module.scss';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+
+interface InputProps extends HTMLInputProps {
+  className?: string
+  value?: string
+  onChange?: (value: string) => void
+  autofocus?: boolean
+}
+
+export const Input = memo((props: InputProps) => {
+  const {
+    className,
+    value,
+    onChange,
+    type = 'text',
+    placeholder,
+    autofocus,
+    ...otherProps
+  } = props;
+
+  const ref = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [caretPostition, SetCaretPosition] = useState(0);
+
+  useEffect(() => {
+    if (autofocus) {
+      setIsFocused(true);
+      ref.current?.focus();
+    }
+  }, [autofocus]);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+    SetCaretPosition(e.target.value.length);
+  };
+
+  const onBlur = () => {
+    setIsFocused(false);
+  };
+
+  const onFocus = () => {
+    setIsFocused(true);
+  };
+
+  const onSelect = (e: any) => {
+    SetCaretPosition(e?.target?.selectionStart || 0);
+  };
+
+  return (
+    <div className={classNames(cls.InputWrapper, {}, [className])}>
+      {placeholder && (
+        <div className={cls.placeholder}>
+          {`${placeholder}>`}
+        </div>
+      )}
+
+      <div className={cls.caretWrapper}>
+        <input
+          ref={ref}
+          type={type}
+          value={value}
+          className={cls.input}
+          onChange={onChangeHandler}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onSelect={onSelect}
+          {...otherProps}
+        />
+        {isFocused && (
+          <span className={cls.caret} style={{ left: `${caretPostition * 9}px` }} />
+        )}
+      </div>
+    </div>
+  );
+});
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { useCallback, useState } from 'react';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,21 +15,21 @@ export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setisAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setisAuthModal((prev) => !prev);
+  const onCloseModal = useCallback(() => {
+    setisAuthModal(false);
+  }, []);
+
+  const onShowModal = useCallback(() => {
+    setisAuthModal(true);
   }, []);
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onToggleModal}>
+      <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onShowModal}>
         {t('Войти')}
       </Button>
 
-      {/* eslint-disable-next-line */}
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Hic impedit quo quae ratione ipsam minima optio rem at quis dolorem.
-      </Modal>
+      <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
     </div>
   );
 };
