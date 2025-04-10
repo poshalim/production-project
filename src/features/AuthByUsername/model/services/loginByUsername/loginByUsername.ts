d0ae48765b38d9a4268bc8949b9acef26817add5
is.kazantcev <is.kazantcev@severstal.com>
2025-04-10 10:07:17 +0300

    Add authByUsername tests

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -25,7 +25,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
     } catch (e) {
       console.log(e);
 
-      return thunkAPI.rejectWithValue('errror');
+      return thunkAPI.rejectWithValue('error');
     }
   },
 );
